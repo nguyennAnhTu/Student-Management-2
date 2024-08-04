cd883627754819e13a8424de0d3b76a4bd0e9bfd
nguyennAnhTu <tuthachdau143@gmail.com>
2024-08-04 22:53:04 +0700

    u

--- src/main/java/Management.java
+++ src/main/java/Management.java
@@ -1,6 +1,6 @@
 public interface Management {
-    void display(String s);
+    void display(String id);
     void add(Student student);
-    void delete(String s);
-    void update(String s);
+    void delete(String id);
+    void update(String id);
 }
